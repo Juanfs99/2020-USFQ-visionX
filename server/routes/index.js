3f64b67d22988256e7c29ea0cc6f00b94bf808b3
Juanfs99 <juanfersubia@gmail.com>
2020-11-28 14:10:34 -0500

    dd

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-import fs from "fs";
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
